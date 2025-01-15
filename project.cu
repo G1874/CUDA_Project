8dc2f0aea97b8389d5877cc737ab403dbb5aadc1
lukosn <l.narog01@gmail.com>
2025-01-15 17:32:36 +0100

    Added data loader

--- project.cu
+++ project.cu
@@ -4,34 +4,44 @@
 #include <stdio.h>
 #include <fstream>
 
-void load_data(const char* filename, float*& data, int rows, int cols)
+float Number_size = 28 * 28;
+
+float* load_data(const char* filename, int size)
 {
     std::ifstream file(filename);
-    if (!file.is_open())
+    if(!file.is_open())
     {
         std::cerr << "Error: file not found" << std::endl;
-        return;
+        return NULL;
     }
 
-    data = new float[rows * cols];
-    for (int i = 0; i < rows * cols; i++)
+    float* data = new float[size];
+    for(int i = 0; i < size; i++)
     {
         file >> data[i];
-        if (file.peek() == ',')
+        if(file.peek() == ',')
             file.ignore();
     }
-
     file.close();
+
+    return data;
 }
 
 
 int main() 
 {
-    float* x_train = nullptr;
+    float* x_train;
+    float* y_train;
+    float* x_test;
+    float* y_test;
 
-    load_data("dataset/y_test.txt", x_train, 100, 1);
+    x_train = load_data("dataset/x_train.txt", 60000 * Number_size);
+    y_test = load_data("dataset/y_train.txt", 60000);
+    x_test = load_data("dataset/x_test.txt", 10000 * Number_size);
+    y_test = load_data("dataset/y_test.txt", 10000);
 
-    if (x_train)
+
+    if(x_train)
     {
         printf("First 100 elements of x_test:\n");
         for (int i = 0; i < 100; i++)
